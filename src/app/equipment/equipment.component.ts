8188509baabe246bd86db09cccf2dd3eea0e85a2
Will Christen <christenwill58@gmail.com>
2022-07-07 20:52:47 -0400

    First commit

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -27,5 +27,8 @@ export class EquipmentComponent implements OnInit {
    ngOnInit() { }
 
    // Code your addItem function here:
-   
+   addItem(equipment: object) {
+    this.cargoHold.push(equipment);
+    this.cargoMass += equipment.mass
+   } 
 }
